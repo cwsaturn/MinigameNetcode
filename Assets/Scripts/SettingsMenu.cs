319289d673731fc3ff82a14e46cc12dd0e311b14
dapb7b <dapb7b@umsystem.edu>
2023-03-21 17:47:17 -0500

    res player pref

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using System;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -19,21 +20,33 @@ public class SettingsMenu : MonoBehaviour
     public Slider announcer;
     public Slider sfx;
 
+    public TMPro.TMP_Dropdown dropdown;
+
     public List<ResItem> resolutions = new List<ResItem>();
     private int selectedResolution;
 
+
     void Start() {
+        //glados variables
         prev_vol = 0;
         init_time = Time.deltaTime;
         volumeUpdate = Time.deltaTime - init_time;
 
+        //audio mixer values
         audioMixer.SetFloat("Music Volume", PlayerPrefs.GetFloat("music_vol", 0));
         audioMixer.SetFloat("Announcer Volume", PlayerPrefs.GetFloat("announcer_vol", 0));
         audioMixer.SetFloat("SFX Volume", PlayerPrefs.GetFloat("sfx_vol", 0));
 
+        //slider values
         music.value = PlayerPrefs.GetFloat("music_vol", 0);
         announcer.value = PlayerPrefs.GetFloat("announcer_vol", 0);
         sfx.value = PlayerPrefs.GetFloat("sfx_vol", 0);
+
+        //resolution
+        int res_index = PlayerPrefs.GetInt("res_index", 0);
+        Screen.SetResolution(resolutions[res_index].horizontal, resolutions[res_index].vertical, false);
+        dropdown.value = res_index;
+
     }
 
     public void SetMusicVolume (float volume)
@@ -67,7 +80,8 @@ public class SettingsMenu : MonoBehaviour
 
     public void ResDropdown(int index)
     {
-        Screen.SetResolution(resolutions[index].horizontal, resolutions[index].vertical, true);
+        Screen.SetResolution(resolutions[index].horizontal, resolutions[index].vertical, false);
+        PlayerPrefs.SetInt("res_index", index);
     }
 }
 
