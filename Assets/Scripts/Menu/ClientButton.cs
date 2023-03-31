d17fe9b49ef1ad22bce74e3bfad87e7bcbc91d80
AnnaM02 <acmkgm@mst.edu>
2023-03-31 17:15:13 -0500

    different local + lan buttons

--- Assets/Scripts/Menu/ClientButton.cs
+++ Assets/Scripts/Menu/ClientButton.cs
@@ -7,11 +7,19 @@ using UnityEngine.UI;
 public class ClientButton : MonoBehaviour
 {
     public GameObject ipInputField;
-
+    public GameObject localButton;
+    public GameObject lanButton; 
     public void OnButtonPress()
     {
+      localButton.GetComponent<Button>().onClick.AddListener(() => SetIpAddress("127.0.0.1"));
+      lanButton.GetComponent<Button>().onClick.AddListener(ShowInputField);
+    }
+   
+    private void ShowInputField()
+    {
+      SetButtonsNotActive();
       ipInputField.SetActive(true);
-      ipInputField.GetComponent<InputField>().onSubmit.AddListener((ipAddr) => SetIpAddress(ipAddr)); //Client inputs host ip address
+      ipInputField.GetComponent<InputField>().onSubmit.AddListener((ipAddr) => SetIpAddress(ipAddr)); //Host enters ip address
     }
 
     private void SetIpAddress(string ipAddr)
@@ -19,5 +27,11 @@ public class ClientButton : MonoBehaviour
       PlayerPrefs.SetString("HostIpAddr", ipAddr);
       PlayerPrefs.SetString("IsHost", "False");
       SceneManager.LoadScene("StartLobby", LoadSceneMode.Single);
+    }
+
+    private void SetButtonsNotActive()
+    {
+      localButton.SetActive(false);
+      lanButton.SetActive(false);
     }
 }
--- Assets/Scripts/Menu/HostButton.cs
+++ Assets/Scripts/Menu/HostButton.cs
@@ -8,17 +8,33 @@ public class HostButton : MonoBehaviour
 {
 
     public GameObject ipInputField;
+    public GameObject localButton;
+    public GameObject lanButton; 
     public void OnButtonPress()
     {
-  
+      localButton.GetComponent<Button>().onClick.AddListener(() => SetIpAddress("127.0.0.1"));
+      lanButton.GetComponent<Button>().onClick.AddListener(ShowInputField);
+    }
+
+    
+    private void ShowInputField()
+    {
+      SetButtonsNotActive();
       ipInputField.SetActive(true);
       ipInputField.GetComponent<InputField>().onSubmit.AddListener((ipAddr) => SetIpAddress(ipAddr)); //Host enters ip address
     }
 
     private void SetIpAddress(string ipAddr)
     {
+      SetButtonsNotActive();
       PlayerPrefs.SetString("HostIpAddr", ipAddr);
       PlayerPrefs.SetString("IsHost", "True");
       SceneManager.LoadScene("StartLobby", LoadSceneMode.Single);
+    }
+
+    private void SetButtonsNotActive()
+    {
+      localButton.SetActive(false);
+      lanButton.SetActive(false);
     }
 }
