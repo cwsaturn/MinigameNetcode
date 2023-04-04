8308c77afebf1f9219c73e94075c5cf4e9f4b470
Jerod <jerodpeterson2@gmail.com>
2023-04-04 16:01:05 -0500

    Kart Game checkpoints

--- Assets/Scripts/Player/KartData.cs
+++ Assets/Scripts/Player/KartData.cs
@@ -8,6 +8,14 @@ public class KartData : NetworkBehaviour
 {
     private float timePassed = 0f;
 
+    private int previousCheckpoint = 0;
+    private int progress = 0;
+
+    [SerializeField]
+    private int checkpointNum = 3;
+    [SerializeField]
+    private int laps = 3;
+
     private TextMeshProUGUI timeText;
     [SerializeField]
     private PlayerScript playerScript;
@@ -34,6 +42,14 @@ public class KartData : NetworkBehaviour
         //SyncNetVariables();
     }
 
+    void localFinish()
+    {
+        playerScript.FinishedServerRpc(-timePassed);
+        GetComponent<KartMovement>().active = false;
+        playerActive = false;
+        timeText.text = timePassed.ToString();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -55,12 +71,26 @@ public class KartData : NetworkBehaviour
 
         if (!playerActive) return;
 
-        if (collision.gameObject.tag == "Finish")
+        if (collision.gameObject.tag != "Checkpoint") return;
+
+        int index = collision.gameObject.GetComponent<Checkpoint>().Index;
+
+        //do nothing if same checkpoint
+        if (index == previousCheckpoint) return;
+
+        if (index == (previousCheckpoint + 1) % checkpointNum)
         {
-            playerScript.FinishedServerRpc(-timePassed);
-            GetComponent<KartMovement>().active = false;
-            playerActive = false;
-            timeText.text = timePassed.ToString();
+            progress += 1;
+        }
+        else
+        {
+            progress -= 1;
+        }
+        previousCheckpoint = index;
+
+        if (progress / checkpointNum >= laps)
+        {
+            localFinish();
         }
     }
 }
