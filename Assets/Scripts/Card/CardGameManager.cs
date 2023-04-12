62941813760916fed307cb4fc37ea8572cc1ef59
Matt Chenot <chenotm76@gmail.com>
2023-04-11 20:05:46 -0500

    working on cards

--- Assets/Scripts/Card/CardGameManager.cs
+++ Assets/Scripts/Card/CardGameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CardGameManager : MonoBehaviour
 {
+    public GameObject[] cards;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +14,7 @@ public class CardGameManager : MonoBehaviour
         int NumPlayers = players.Length;
 
         Debug.Log("Num Players: " + NumPlayers);
+
     }
 
     // Update is called once per frame
