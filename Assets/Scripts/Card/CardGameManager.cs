b70b77ddd125cf804bd119ffee860b75fc57a721
Cullin <messiart58@gmail.com>
2023-04-13 15:26:26 -0500

    card game manager seed networked

--- Assets/Scripts/Card/CardGameManager.cs
+++ Assets/Scripts/Card/CardGameManager.cs
@@ -1,20 +1,35 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
-public class CardGameManager : MonoBehaviour
+public class CardGameManager : NetworkBehaviour
 {
     public GameObject[] cards;
 
     public int[] cardValues;
 
+    public NetworkVariable<int> randomSeverSeed = new NetworkVariable<int>(0);
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        if(IsClient)
+            return;
+
+        
+        randomSeverSeed.Value = Random.Range(-9999, 9999);
+
+
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         int NumPlayers = players.Length;
 
+        Random.InitState(randomSeverSeed.Value);    // seed random num
         cardValues = new int[cards.Length];
         int i = 0;
         foreach(GameObject card in cards)
@@ -31,5 +46,42 @@ public class CardGameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    [ServerRpc]
+    void ScoreSetServerRpc(ServerRpcParams rpcParams = default)
+    {
+        randomSeverSeed.Value++;
+    }
+
+
+    public override void OnNetworkSpawn()
+    {
+        // Subscribe to value changes
+        randomSeverSeed.OnValueChanged += OnRandSeedChange;
+    }
+
+
+    public override void OnNetworkDespawn()
+    {
+        // Unsubscribe to value changes
+        randomSeverSeed.OnValueChanged -= OnRandSeedChange;
+    }
+
+    public void OnRandSeedChange(int previous, int current)
+    {
+        Random.InitState(randomSeverSeed.Value);    // seed random num
+
+        cardValues = new int[cards.Length];
+        int i = 0;
+        foreach(GameObject card in cards)
+        {
+            TMP_Text cardText = card.GetComponentInChildren<TMP_Text>();
+            int cardVal = Random.Range(0, 30);  // 0 through 29
+            cardValues[i] = cardVal;
+            cardText.text = cardVal.ToString();
+            i++;
+        }
     }
 }
