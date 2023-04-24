53706f5eb4370d2c6d41f135236cb77fa0e207ae
Matt Chenot <chenotm76@gmail.com>
2023-04-24 03:14:43 -0500

    Basics for chains of games

--- Assets/Scripts/UI/GameStartButtons.cs
+++ Assets/Scripts/UI/GameStartButtons.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameStartButtons : MonoBehaviour
 {
     private static string MenuState = "BaseMenu";
+    private static int GamesLeftToPlay = 0;
 
     void OnGUI()
     {
@@ -24,15 +25,34 @@ public class GameStartButtons : MonoBehaviour
         else if (MenuState == "SequenceMenu")  // Buttons for game sequence submenu
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            StartButton("IDK Yey 1", "IDK Yet 1");
+            StartButton("Sequence3", "Play 3 Random Games");
             GUILayout.EndArea();
 
             GUILayout.BeginArea(new Rect(10, 40, 300, 300));
-            StartButton("IDK Yey 2", "IDK Yet 2");
+            StartButton("Sequence5", "Play 5 Random Games");
             GUILayout.EndArea();
 
             GUILayout.BeginArea(new Rect(10, 70, 300, 300));
+            StartButton("Sequence7", "Play 7 Random Games");
+            GUILayout.EndArea();
+
+            GUILayout.BeginArea(new Rect(10, 100, 300, 300));
+            StartButton("This Doesn't do Anything yet", "Custom");
+            GUILayout.EndArea();
+
+            GUILayout.BeginArea(new Rect(10, 130, 300, 300));
             StartButton("LeaveSequence", "Back");
+            GUILayout.EndArea();
+        }
+
+        else if (MenuState == "MidSequenceMenu")
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            StartButton("ContinueSequence", "Continue");
+            GUILayout.EndArea();
+
+            GUILayout.BeginArea(new Rect(10, 40, 300, 300));
+            StartButton("EndSequence", "End");
             GUILayout.EndArea();
         }
 
@@ -77,27 +97,46 @@ public class GameStartButtons : MonoBehaviour
         var mode = NetworkManager.Singleton.IsHost ?
             "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
-        if (GUILayout.Button(buttonText))
+        if (mode == "Host")  // Only host makes game choices
         {
-            // Flags for menu navigation
-            if (sceneName == "StartFreePlay") {MenuState = "FreePlay";}
-            else if (sceneName == "LeaveFreePlay" || sceneName == "LeaveSequence") {MenuState = "BaseMenu";}
-            else if (sceneName == "StartSequence") {MenuState = "SequenceMenu";}
-
-            // Game selection and transition
-            else
+            if (GUILayout.Button(buttonText))
             {
-                if (sceneName == "RandomGame")  // Get a random game to start if needed
+                // Flags for menu navigation
+                if (sceneName == "StartFreePlay") {MenuState = "FreePlay";}
+                else if (sceneName == "LeaveFreePlay" || sceneName == "LeaveSequence") {MenuState = "BaseMenu";}
+                else if (sceneName == "StartSequence") {MenuState = "SequenceMenu";}
+
+                else if (sceneName == "EndSequence")  // Quiting a sequence of games early
                 {
-                    string[] games = {"Platformer2", "Kart", "ShellGame", "Card", "TargetGame", "Shootemup"};
-                    sceneName = games[Random.Range(0, games.Length)];  // Get random game from list
-                    Debug.Log("Random scene: " + sceneName);
+                    MenuState = "BaseMenu";
+                    GamesLeftToPlay = 0;
+                }
+
+                // Game selection and transition
+                else
+                {
+                    if (sceneName == "Sequence3") {GamesLeftToPlay = 3;}  // Set count for sequence play
+                    else if (sceneName == "Sequence5") {GamesLeftToPlay = 5;}
+                    else if (sceneName == "Sequence7") {GamesLeftToPlay = 7;}
+
+                    if (sceneName == "RandomGame" || sceneName == "ContinueSequence" || GamesLeftToPlay > 0)  // Get a random game to start if needed
+                    {
+                        string[] games = {"Platformer2", "Kart", "ShellGame", "Card", "TargetGame", "Shootemup"};
+                        sceneName = games[Random.Range(0, games.Length)];  // Get random game from list
+                    }
+
+                    if (GamesLeftToPlay > 0)  // If currently doing a sequence of games
+                    {
+                        MenuState = "MidSequenceMenu";
+                        GamesLeftToPlay -= 1;
+                        Debug.Log("Games left to play: " + GamesLeftToPlay);
+
+                        if (GamesLeftToPlay <= 0) {MenuState = "BaseMenu";}  // Return to base menu when done with the sequence of games
+                    }
+            
                     NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
                 }
-                
-                NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
             }
-            
         }
     }
 }
