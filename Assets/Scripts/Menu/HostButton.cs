df79b00271a1dc69a7663c9e85b8ab362fd48471
AnnaM02 <acmkgm@mst.edu>
2023-04-24 13:16:01 -0500

    first shot at adding port forwarding functionality

--- Assets/Scripts/Menu/HostButton.cs
+++ Assets/Scripts/Menu/HostButton.cs
@@ -6,22 +6,35 @@ using UnityEngine.UI;
 
 public class HostButton : MonoBehaviour
 {
-
     public GameObject ipInputField;
+    public GameObject portInputField; 
+
     public GameObject localButton;
     public GameObject lanButton; 
+    public GameObject portFwdButton; 
+    
+    private bool usePortForwarding = false; 
+
     public void OnButtonPress()
     {
       localButton.GetComponent<Button>().onClick.AddListener(() => SetIpAddress("127.0.0.1"));
-      lanButton.GetComponent<Button>().onClick.AddListener(ShowInputField);
+      lanButton.GetComponent<Button>().onClick.AddListener(() => ShowIPInputField(false));
+      portFwdButton.GetComponent<Button>().onClick.AddListener(() => ShowIPInputField(true));
     }
 
-    
-    private void ShowInputField()
+    private void ShowIPInputField(bool portForwarding)
     {
+      if(portForwarding) { usePortForwarding = true;}
       SetButtonsNotActive();
       ipInputField.SetActive(true);
-      ipInputField.GetComponent<InputField>().onSubmit.AddListener((ipAddr) => SetIpAddress(ipAddr)); //Host enters ip address
+      ipInputField.GetComponent<InputField>().onSubmit.AddListener((ipAddr) => SetIpAddress(ipAddr)); 
+    }
+
+    private void ShowPortInputField()
+    {
+      ipInputField.SetActive(false);
+      portInputField.SetActive(true);
+      portInputField.GetComponent<InputField>().onSubmit.AddListener((port) => SetPort(port)); //Host enters port
     }
 
     private void SetIpAddress(string ipAddr)
@@ -29,12 +42,25 @@ public class HostButton : MonoBehaviour
       SetButtonsNotActive();
       PlayerPrefs.SetString("HostIpAddr", ipAddr);
       PlayerPrefs.SetString("IsHost", "True");
+      if(!usePortForwarding)
+      {
+        PlayerPrefs.SetString("HostPort", "7777");
+        SceneManager.LoadScene("StartLobby", LoadSceneMode.Single);
+      }
+      else{ShowPortInputField();}
+    }
+
+    private void SetPort(string port)
+    {
+      PlayerPrefs.SetString("HostPort", port);
       SceneManager.LoadScene("StartLobby", LoadSceneMode.Single);
     }
+    
 
     private void SetButtonsNotActive()
     {
       localButton.SetActive(false);
       lanButton.SetActive(false);
+      portFwdButton.SetActive(false);
     }
 }
